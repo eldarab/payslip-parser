04b53a62188b7d2f082fc92a9a3e0bff9e3d740b
Eldar Abraham <eldar.abraham@gmail.com>
2023-06-09 19:05:34 +0300

    Add support for entire directory parsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 
+from business_logic.payslip_directory_parser import PayslipDirectoryParser
 from configuration.loader import load_config
 from idf.idf_payslip_parser import IDFPayslipParser
 
@@ -7,13 +8,16 @@ from idf.idf_payslip_parser import IDFPayslipParser
 def main(args):
     config = load_config(args.config_path)
     idf_payslip_parser = IDFPayslipParser(config)
-    parsed_payslip = idf_payslip_parser.parse_payslip(args.payslip_path)
-    print()
+    idf_directory_parser = PayslipDirectoryParser(idf_payslip_parser)
+    df = idf_directory_parser.directory_to_dataframe(args.directory_path)
+    print(df)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--payslip_path', type=str, default='./86901611520231.pdf')
+    parser.add_argument('--payslip_path', type=str,
+                        default='./G:/My Drive/Personal/Finance/Paychecks/IDF/86901611520231.pdf')
+    parser.add_argument('--directory_path', type=str, default='G:/My Drive/Personal/Finance/Paychecks/IDF')
     parser.add_argument('--config_path', type=str, default='./idf/config.yaml')
     main_args = parser.parse_args()
 
