fa73dab1b88520157291e42c8c5d3be97d524afc
Eldar Abraham <eldar.abraham@gmail.com>
2023-11-12 20:43:36 +0200

    Add version to setup

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='payslip_parser',
+    version='0.1.1',
     description='Parse your payslips from PDFs to pandas dataframes.',
     author='Eldar Abraham',
     url='https://github.com/eldarab/payslip-parser',
